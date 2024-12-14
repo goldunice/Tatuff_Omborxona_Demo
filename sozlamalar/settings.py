027b95c5d7a374aeb97b71a602e6b8b3791b29e7
goldunice <AkmaljonGold@gmail.com>
2024-12-14 11:16:12 +0500

    First commit

--- sozlamalar/settings.py
+++ sozlamalar/settings.py
@@ -217,7 +217,7 @@ JAZZMIN_SETTINGS = {
     # - vertical_tabs
     # - collapsible
     # - carousel
-    "changeform_format": "vertical_tabs",
+    "changeform_format": "horizontal_tabs",
     # override change forms on a per modeladmin basis
     "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
     # Add a language dropdown into the admin
